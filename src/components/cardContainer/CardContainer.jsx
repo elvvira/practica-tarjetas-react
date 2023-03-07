b518e7f2d236ce14e44b567bce17534b69a2ec5b
Daniel Jiménez <>
2023-03-07 12:43:33 +0100

    final

--- src/components/cardContainer/CardContainer.jsx
+++ src/components/cardContainer/CardContainer.jsx
@@ -1,27 +1,21 @@
+import { cards } from '../../constants/cards';
 import Card from '../card/Card';
 import { StyledContainer } from './styles';
 
 const CardContainer = () => {
 	return (
 		<StyledContainer>
-			<Card
-				image='sedan-car.svg'
-				color='orange'
-				colorAfter='#c26101'
-				title='SEDAN'
-			/>
-			<Card
-				image='suv-car.svg'
-				color='blue'
-				colorAfter='#012088'
-				title='SUVs'
-			/>
-			<Card
-				image='luxury-car.svg'
-				color='green'
-				colorAfter='#044e00'
-				title='LUXURY'
-			/>
+			{cards.map(card => {
+				return (
+					<Card
+						key={card.id}
+						image={card.image}
+						color={card.color}
+						colorAfter={card.colorAfter}
+						title={card.title}
+					/>
+				);
+			})}
 		</StyledContainer>
 	);
 };
--- src/constants/cards.js
+++ src/constants/cards.js
@@ -1,8 +1,25 @@
 const cards = [
-	(card = {
+	{
+		id: 1,
 		image: 'sedan-car.svg',
 		color: 'orange',
 		colorAfter: '#c26101',
 		title: 'SEDAN'
-	})
+	},
+	{
+		id: 2,
+		image: 'suv-car.svg',
+		color: 'blue',
+		colorAfter: '#012088',
+		title: 'SUVs'
+	},
+	{
+		id: 3,
+		image: 'luxury-car.svg',
+		color: 'green',
+		colorAfter: '#044e00',
+		title: 'LUXURY'
+	}
 ];
+
+export { cards };
